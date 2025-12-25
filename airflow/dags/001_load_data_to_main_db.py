c67ef78562d24fade2324886156b223021dc325b
Nikita Evseev <naevseev.work@gmail.com>
2025-12-25 19:56:47 +0300

    delete dag retry

--- airflow/dags/001_load_data_to_main_db.py
+++ airflow/dags/001_load_data_to_main_db.py
@@ -23,8 +23,7 @@ default_args = {
     'start_date': datetime(2025, 1, 1),
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 0,
 }
 
 # Определение DAG
